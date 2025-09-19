6bd1f2e7505023d75a26e1ffdee69d61bf82624e
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-09-19 19:50:26 +0200

    Move component monitoring to its own package
    Agent monitoring has two separate functions - implementing the control
    plane monitoring server and self-monitoring for components. Having both
    in the same packages caused a dependency cycle involving the otel
    translation package. Resolve this by putting component monitoring in a
    subpackage

--- internal/pkg/agent/cmd/inspect.go
+++ internal/pkg/agent/cmd/inspect.go
@@ -14,11 +14,12 @@ import (
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
 
+	componentmonitoring "github.com/elastic/elastic-agent/internal/pkg/agent/application/monitoring/component"
+
 	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-libs/service"
 
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/info"
-	"github.com/elastic/elastic-agent/internal/pkg/agent/application/monitoring"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/application/paths"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/configuration"
 	"github.com/elastic/elastic-agent/internal/pkg/agent/errors"
@@ -409,7 +410,7 @@ func getMonitoringFn(ctx context.Context, cfg map[string]interface{}) (component
 		return nil, fmt.Errorf("could not load agent info: %w", err)
 	}
 
-	monitor := monitoring.New(agentCfg.Settings.V1MonitoringEnabled, agentCfg.Settings.DownloadConfig.OS(), agentCfg.Settings.MonitoringConfig, agentInfo)
+	monitor := componentmonitoring.New(agentCfg.Settings.V1MonitoringEnabled, agentCfg.Settings.DownloadConfig.OS(), agentCfg.Settings.MonitoringConfig, agentInfo)
 	return monitor.MonitoringConfig, nil
 }
 
